a9d587c2366f81bcc59868b2461591dce73d7cdd
Tom Dakin <TDakin@seamsglobal.com>
2019-07-17 15:56:32 +0100

    write basic analysis for movies, add prompt for user to add own code

---
+++ src/movies.R
@@ -0,0 +1,63 @@
+#!Rscript 
+
+# *** A script to analyse some movie data and report team members' favourites **
+# Project Name: git_101
+# Author: Tom Dakin 
+# Contact: t.dakin@seamsltd.com
+# Creation Date: 2019/07/17
+#
+# ******************************************************************************
+
+# --- Initialise ---------------------------------------------------------------
+rm(list=ls())
+library(tidyverse)
+library(lubridate)
+
+
+# --- Load data ----------------------------------------------------------------
+movies <- 
+  as_tibble(read.csv("data\\processed\\movies.csv", stringsAsFactors=FALSE))
+
+
+# --- Preprocess ---------------------------------------------------------------
+movies <- 
+  movies %>% 
+  mutate(date = ymd(release_date))
+
+
+# --- Analysis -----------------------------------------------------------------
+#report number of movies
+movies %>% 
+  nrow %>% 
+  paste("The number of movies in the collection is:", .) %>% 
+  print()
+
+movies %>% 
+  arrange(date) %>% 
+  select(title) %>% 
+  head(1) %>% 
+  paste("The earliest movie in the dataset is:", .) %>% 
+  print()
+
+movies %>% 
+  arrange(desc(revenue)) %>% 
+  select(title) %>% 
+  head(1) %>% 
+  paste("The highest grossing movie in the dataset is:", .) %>% 
+  print()
+
+movies %>% 
+  arrange(desc(vote_average)) %>% 
+  select(title) %>% 
+  head(1) %>% 
+  paste("The highest rated movie in the dataset is:", .) %>% 
+  print()
+
+
+# --- Report favourites --------------------------------------------------------
+print("Tom's favourite movie is 'The Big Lebowski'")
+
+# TODO Declare your own favourite with a print statement here...
+
+
+# --- End ----------------------------------------------------------------------
